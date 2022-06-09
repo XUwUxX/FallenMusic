a70b50bb4fc1fc797785746fb0dca0f78ab10ad3
Lisa Admin Github ✔ <93268631+XUwUxX@users.noreply.github.com>
2022-06-09 14:43:01 +0700

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -44,7 +44,7 @@ async def start_(client: Client, message: Message):
                         "🔎 ɪɴʟɪɴᴇ 🔎", switch_inline_query_current_chat=""
                     ),
                     InlineKeyboardButton(
-                        "🤯 sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ​ 🤯", url="https://github.com/AnonymousR1025/FallenMusic"
+                        "🤯 ᴄᴏᴅᴇ​ 🤯", url="https://t.me/{me}"
                     )]
             ]
        ),
